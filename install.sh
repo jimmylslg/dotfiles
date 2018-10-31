b243d3cf8ba6b02ffca2caa58b2a97b33f3d46c1
jimmylslg <jimmypan199357@gmail.com>
2018-10-31 05:58:52 +0000

    enhanced install.sh

--- install.sh
+++ install.sh
@@ -5,19 +5,51 @@ echo "============= Dotfiles ============="
 echo "" 
 
 mkdir -p ~/.dotfiles_bak
+mkdir -p ~/.vim/undodir
 
 echo ""
 echo "*** Backing up existing dotfiles"
 echo "" 
 
 cp ~/.tmux.conf ~/.dotfiles_bak
+unlink ~/.tmux.conf 
 cp ~/.vimrc ~/.dotfiles_bak
+unlink ~/.vimrc
 cp ~/.zshrc ~/.dotfiles_bak
+unlink ~/.zshrc
 
 echo ""
 echo "*** Finished bakcing up"
 echo "" 
 
+
+echo ""
+echo "============= Plugins ============="
+echo "" 
+ 
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh::g' | sed 's:chsh -s .*$::g')"
+ 
+export ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
+export SHELL=/bin/zsh
+
+#Plugin Installs
+ 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
+
+# zsh_autosuggestions
+# zsh_autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+ 
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+ 
+# autojump
+git clone git://github.com/wting/autojump.git $ZSH_CUSTOM/plugins/autojump 
+cd  $ZSH_CUSTOM/plugins/autojump && ./uninstall.py && ./install.py
+cd -
+ 
 echo ""
 echo "*** Copying latest dotfiles"
 echo "" 
@@ -30,28 +62,9 @@ echo ""
 echo "*** dotfiles are the latest now!"
 echo "" 
 
-# echo ""
-# echo "============= Plugins ============="
-# echo "" 
-# 
-# export ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
-# 
-# # Install oh-my-zsh
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# 
-# #Plugin Installs
-# 
-# # zsh_autosuggestions
-# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-# 
-# # zsh-syntax-highlighting
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-# 
-# # autojump
-# git clone git://github.com/wting/autojump.git $ZSH_CUSTOM/plugins/autojump 
-# cd  $ZSH_CUSTOM/plugins/autojump && ./uninstall.py && ./install.py
-# cd -
-# 
-# echo ""
-# echo "*** Finished. Please reload config."
-# echo ""
+echo | echo | vim +PluginInstall +qall
+chsh -s /bin/zsh
+
+echo ""
+echo "*** Finished. Please kill the terminal to reload config."
+echo ""
