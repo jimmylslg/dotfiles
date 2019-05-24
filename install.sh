c8847b9125d010669373192f3c250116c1c0ebe0
jimmylslg <jimmypan199357@gmail.com>
2019-05-24 14:02:51 -0700

    added tmux plugins

--- install.sh
+++ install.sh
@@ -69,6 +69,9 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
  
 # zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
  
 # autojump
 git clone git://github.com/wting/autojump.git $ZSH_CUSTOM/plugins/autojump 
